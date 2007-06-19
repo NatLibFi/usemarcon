31829c871fdc5fa27bd0a8379733d17a317de6f9
ermaijal <ermaijal@1b8ed345-9043-4c14-8c4f-2bb4a3fd8ec5>
2007-06-19 10:19:17 +0000

    Added command to fix state buffer deletion.

--- library/MakeParser.bat
+++ library/MakeParser.bat
@@ -5,7 +5,10 @@ perl ../bison+.pl --input analyse.y --class MarcParser --cpp ytab.cpp --hdr ytab
 if errorlevel 1 goto bisonerr
 echo Creating Scanner..
 flex -L -olex.yy.cpp Analyse.l
+echo Fixing Scanner Destructor..
+perl -pi -e 's/delete yy_state_buf;/delete []yy_state_buf;/' lex.yy.cpp
 if errorlevel 1 goto flexerr
+echo All done.
 goto end
 :bisonerr
 echo Parser creation failed
