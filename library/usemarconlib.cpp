450d25617dca3eacbca56ffba6f340b71507f4bb
ermaijal <ermaijal@1b8ed345-9043-4c14-8c4f-2bb4a3fd8ec5>
2007-08-09 14:13:46 +0000

    New API.

---
+++ library/usemarconlib.cpp
@@ -0,0 +1,136 @@
+/*
+ *  USEMARCON Software - Command Line version
+ *  Copyright The British Library, The USEMarcon Consortium, 1995-2000
+ *  Adapted by Crossnet Systems Limited - British Library Contract No. BSDS 851
+ *
+ *  Adapted by ATP Library Systems Ltd, Finland, 2002-2004
+ *  Adapted by The National Library of Finland, 2004-2007
+ *
+ *  File:  usemarconlib.cpp
+ *
+ *  implements an interface to the conversion
+ *
+ */
+
+
+#include <stdlib.h>
+#include "tmpplctn.h"
+#include "usemarconlib.h"
+
+
+
+Usemarcon::Usemarcon() : m_record(NULL), m_length(0), m_iniFileName(NULL),
+    m_inputMarcFileName(NULL), m_outputMarcFileName(NULL),
+    m_initialized(false), m_interactive(false), m_force_verbose(false), 
+    m_disable_character_conversion(false)
+{
+    m_lastErrorMessage[0] = '\0';
+
+    m_application = new TUMApplication();
+}
+
+Usemarcon::~Usemarcon()
+{
+    free(m_record);
+
+    if (m_iniFileName)
+        free(m_iniFileName);
+
+    if (m_inputMarcFileName)
+        free(m_inputMarcFileName);
+        
+    if (m_outputMarcFileName)
+        free(m_outputMarcFileName);
+
+    delete m_application;
+}
+
+void Usemarcon::SetInteractive(bool a_value)
+{
+    if (m_interactive == a_value)
+        return;
+
+    m_interactive = a_value;
+    m_initialized = false;
+}
+
+void Usemarcon::SetMarcRecord(const char *a_record, int a_length)
+{
+    free(m_record);
+
+    m_length = a_length;
+    m_record = reinterpret_cast<char *>(malloc(m_length + 1));
+    memset(m_record, 0, m_length + 1);
+    memcpy(m_record, a_record, m_length);
+}
+
+void Usemarcon::GetMarcRecord(char *&a_record, int &a_length)
+{
+    a_record = m_record ? strdup(m_record) : NULL;
+    a_length = m_length;
+}
+
+void Usemarcon::SetIniFileName(const char *a_iniFile)
+{
+    if (m_iniFileName && !strcmp(m_iniFileName, a_iniFile))
+        return;
+    if (m_iniFileName)
+        free(m_iniFileName);
+    m_iniFileName = strdup(a_iniFile);
+    m_initialized = false;
+}
+
+void Usemarcon::SetInputMarcFileName(const char *a_filename)
+{
+    if (m_inputMarcFileName)
+        free(m_inputMarcFileName);
+    m_inputMarcFileName = strdup(a_filename);
+}
+
+void Usemarcon::SetOutputMarcFileName(const char *a_filename)
+{
+    if (m_outputMarcFileName)
+        free(m_outputMarcFileName);
+    m_outputMarcFileName = strdup(a_filename);
+}
+
+void Usemarcon::SetLastErrorMessage(const char *a_msg)
+{
+    strncpy(m_lastErrorMessage, a_msg, 255);
+    m_lastErrorMessage[254] = '\0';
+}
+
+int Usemarcon::Convert()
+{
+    int res = 0;
+
+    TUMApplication *application = reinterpret_cast<TUMApplication*>(m_application);
+    if (!m_initialized)
+    {
+        res = application->StartUp(m_iniFileName, m_interactive,
+            m_record, m_length, m_force_verbose, m_inputMarcFileName,
+            m_outputMarcFileName, m_disable_character_conversion);
+        if (!res)
+            m_initialized = true;
+    }
+    if (!res)
+    {
+        res = application->Convert();
+        if (m_length)
+        {
+            free(m_record);
+            application->GetMarcRecord(m_record, m_length);
+        }
+        application->End();
+    }
+
+    if (application->GetErrorHandler())
+        SetLastErrorMessage(application->GetErrorHandler()->GetLastErrorMessage());
+
+    if (m_interactive)
+    {
+        printf("\n");
+    }
+        
+    return res;
+}
---
+++ library/usemarconlib.h
@@ -0,0 +1,81 @@
+/* 
+ *  USEMARCON Software - Command Line version
+ *  
+ *  File:  usemarconlib.h
+ *
+ *
+ 
+CLASS
+    CUsemarcon
+
+    implements an interface to the conversion
+
+OVERVIEW TEXT
+    USEMARCON Software - Command Line version
+    Copyright The British Library, The USEMarcon Consortium, 1995-2000
+    Adapted by ATP Library Systems Ltd, Finland, 2002-2004
+    Adapted by The National Library of Finland, 2004-2007
+
+AUTHOR
+    Crossnet Systems Limited
+    ATP Library Systems Limited
+    The National Library of Finland
+
+*/
+
+#ifndef usemarconlib_h
+#define usemarconlib_h
+
+class Usemarcon 
+{
+private:
+    void *m_application;
+    bool m_interactive;
+    char *m_record;
+    int m_length;
+    char m_lastErrorMessage[255];
+    bool m_initialized;
+    bool m_force_verbose;
+    bool m_disable_character_conversion;
+    char *m_iniFileName;
+    char *m_inputMarcFileName;
+    char *m_outputMarcFileName;
+    
+public:
+    Usemarcon(); 
+
+    ~Usemarcon();
+
+    void SetForceVerbose(bool a_value) { m_force_verbose = a_value; }
+    bool GetForceVerbose() { return m_force_verbose; }
+
+    void SetDisableCharacterConversion(bool a_value) { m_disable_character_conversion = a_value; }
+    bool GetDisableCharacterConversion() { return m_disable_character_conversion; }
+
+    void SetInteractive(bool a_value);
+    bool GetInteractive() { return m_interactive; }
+
+    bool GetMarcRecordAvailable() { return m_record ? true : false; }
+
+    long GetMarcRecordLength() { return m_length; }
+
+    void SetMarcRecord(const char *a_record, int a_length);
+    void GetMarcRecord(char *&a_record, int &a_length);
+
+    const char *GetIniFileName() { return m_iniFileName; }
+
+    void SetIniFileName(const char *a_iniFile);
+
+    void SetInputMarcFileName(const char *a_filename);
+    const char *GetInputMarcFileName() { return m_inputMarcFileName; }
+
+    void SetOutputMarcFileName(const char *a_filename);
+    const char *GetOutputMarcFileName() { return m_outputMarcFileName; }
+    
+    void SetLastErrorMessage(const char *a_msg);
+    char *GetLastErrorMessage() { return m_lastErrorMessage; }
+
+    int Convert();
+};
+
+#endif
